61f2463a9ab5ba961078da9322c3cba34b841128
Yesenia Urdiales <Yeseurdiales726@gmail.com>
2021-11-23 18:36:21 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+  function introductionWithLanguage(name, language) {
+    console.log(`The two values are ${name} and ${language}.`);
+  }
+  function introductionWithLanguage(language  = "Name") {
+    console.log(`Language, ${Name}!`);
+  }
